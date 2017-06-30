facdd1575be4120ffff2a13aeb646d1b7c0f9d9d
Floris Breggeman <floris@THE-2016-MAC.local>
2017-06-30 15:21:57 +0200

    added 3x3 squares to constraints

--- sudoku.py
+++ sudoku.py
@@ -28,6 +28,69 @@ def rowtest(template, answer, rownum):
 			for column in range(0,9):
 				if template[row][column] == answer[column]:
 					return False
+	#check for 3x3 squares
+	if rownum < 3:
+		for row in range(0,3):
+			for column in range (0,3):
+				if template[row][column] != 'x':
+					for matchrow in range(row, 3):
+						for matchcolumn in range(0, 3):
+							if (template[row][column] == template[matchrow][matchcolumn]) & (row != matchrow) & (column != matchcolumn):
+								return False
+			for column in range(3,6):
+				if template[row][column] != 'x':
+					for matchrow in range(row, 3):
+						for matchcolumn in range(3, 6):
+							if (template[row][column] == template[matchrow][matchcolumn]) & (row != matchrow) & (column != matchcolumn):
+								return False
+			for column in range(6,9):
+				if template[row][column] != 'x':
+					for matchrow in range(row, 3):
+						for matchcolumn in range(6,9):
+							if (template[row][column] == template[matchrow][matchcolumn]) & (row != matchrow) & (column != matchcolumn):
+								return False
+	elif rownum >= 3 & rownum < 6:
+		for row in range(3,6):
+			for column in range(0,3):
+				if template[row][column] != 'x':
+					for matchrow in range(row, 6):
+						for matchcolumn in range(0,3):
+							if (template[row][column] == template[matchrow][matchcolumn]) & (row != matchrow) & (column != matchcolumn):
+								return False
+			for column in range(3,6):
+				if template[row][column] != 'x':
+					for matchrow in range(row, 6):
+						for matchcolumn in range(3,6):
+							if (template[row][column] == template[matchrow][matchcolumn]) & (row != matchrow) & (column != matchcolumn):
+								return False
+			for column in range(6,9):
+				if template[row][column] != 'x':
+					for matchrow in range(row, 6):
+						for matchcolumn in range(6,9):
+							if (template[row][column] == template[matchrow][matchcolumn]) & (row != matchrow) & (column != matchcolumn):
+								return False
+
+	elif rownum >= 6:
+		for row in range(6,9):
+			for column in range(0,3):
+				if template[row][column] != 'x':
+					for matchrow in range(row, 9):
+						for matchcolumn in range(0,3):
+							if (template[row][column] == template[matchrow][matchcolumn]) & (row != matchrow) & (column != matchcolumn):
+								return False
+			for column in range(3,6):
+				if template[row][column] != 'x':
+					for matchrow in range(row, 9):
+						for matchcolumn in range(3,6):
+							if (template[row][column] == template[matchrow][matchcolumn]) & (row != matchrow) & (column != matchcolumn):
+								return False
+			for column in range(6,9):
+				if template[row][column] != 'x':
+					for matchrow in range(row, 9):
+						for matchcolumn in range(6,9):
+							if (template[row][column] == template[matchrow][matchcolumn]) & (row != matchrow) & (column != matchcolumn):
+								return False
+
 	return True
 			
 def findleast(sudoku, exclude = []):
@@ -104,6 +167,7 @@ missingnums = findmissing(sudoku[startrow])
 #generate permutations for startrow
 permutations = itertools.permutations(missingnums)
 for permutation in permutations:
+	# print "l1", startrow, permutation
 	#start with a copy to preserve original
 	template = list(sudoku)
 
@@ -116,6 +180,7 @@ for permutation in permutations:
 		#generate permutations for row 2
 		permutations2 = itertools.permutations(missingnums2)
 		for permutation2 in permutations2:
+			# print "l2", row2, permutation2
 			template2 = list(template)
 			if rowtest(template2, inject(template2[row2], permutation2), row2):
 				template2[row2] = inject(template2[row2], permutation2)
@@ -124,6 +189,7 @@ for permutation in permutations:
 				missingnums3 = findmissing(template2[row3])
 				permutations3 = itertools.permutations(missingnums3)
 				for permutation3 in permutations3:
+					# print "l3", row3, permutation3
 					template3 = list(template2)
 					if rowtest(template3, inject(template3[row3], permutation3), row3):
 						template3[row3] = inject(template3[row3], permutation3)
@@ -132,6 +198,7 @@ for permutation in permutations:
 						missingnums4 = findmissing(template3[row4])
 						permutations4 = itertools.permutations(missingnums4)
 						for permutation4 in permutations4:
+							# print "l4", row4, permutation4
 							template4 = list(template3)
 							if rowtest(template4, inject(template4[row4], permutation4), row4):
 								template4[row4] = inject(template4[row4], permutation4)
@@ -140,6 +207,7 @@ for permutation in permutations:
 								missingnums5 = findmissing(template4[row5])
 								permutations5 = itertools.permutations(missingnums5)
 								for permutation5 in permutations5:
+									# print "l5", row5, permutation5
 									template5 = list(template4)
 									if rowtest(template5, inject(template5[row5], permutation5), row5):
 										template5[row5] = inject(template5[row5], permutation5)
@@ -148,6 +216,7 @@ for permutation in permutations:
 										missingnums6 = findmissing(template5[row6])
 										permutations6 = itertools.permutations(missingnums6)
 										for permutation6 in permutations6:
+											# print "l6", row6, permutation6
 											template6 = list(template5)
 											if rowtest(template6, inject(template6[row6], permutation6), row6):
 												template6[row6] = inject(template6[row6], permutation6)
@@ -156,6 +225,7 @@ for permutation in permutations:
 												missingnums7 = findmissing(template6[row7])
 												permutations7 = itertools.permutations(missingnums7)
 												for permutation7 in permutations7:
+													# print "l7", row7, permutation7
 													template7 = list(template6)
 													if rowtest(template7, inject(template7[row7], permutation7), row7):
 														template7[row7] = inject(template7[row7], permutation7)
@@ -164,6 +234,7 @@ for permutation in permutations:
 														missingnums8 = findmissing(template7[row8])
 														permutations8 = itertools.permutations(missingnums8)
 														for permutation8 in permutations8:
+															# print "l8", row8, permutation8
 															template8 = list(template7)
 															if rowtest(template8, inject(template8[row8], permutation8), row8):
 																template8[row8] = inject(template8[row8], permutation8)
@@ -172,6 +243,7 @@ for permutation in permutations:
 																missingnums9 = findmissing(template8[row9])
 																permutations9 = itertools.permutations(missingnums9)
 																for permutation9 in permutations9:
+																	# print "l9", row9, permutation9
 																	template9 = list(template8)
 																	if rowtest(template9, inject(template9[row9], permutation9), row9):
 																		template9[row9] = inject(template9[row9], permutation9)
